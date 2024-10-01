4f6e597d5b2e9b05f6b69041749a0a458e4657cb
shaluydv01 <ydvshalu420@gmail.com>
2024-10-02 00:31:36 +0530

    First commit

--- moviezone/src/MovieZone/Movies.jsx
+++ moviezone/src/MovieZone/Movies.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { movies } from "./data.js"
+import React, {useState} from 'react'
+import {movies} from "./data.js";
 
-export const Movies = () => {
+const Movies = () => {
     const [movieList, setMovieList] = useState(movies);
   return (
-    <div>
+    <div 
+    style={{
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        flexWrap:'wrap',
+        gap: '2rem',
+        width:'1200px',
+        // backgroundColor:'black',
+        textAlign:'center',
+        margin: 'auto'
+        }}>
         {movieList.map((data) => (
-            <div key={data.id}>
-                <div>
-                    <img src={data.poster_path} alt="" style={{width:'250px'}}/>
+            <div key={data.id} style={{maxWidth:'250px'}}>
+                <div style={{padding: '10px'}} className='hover_effect'>
+                    <img src={data.poster_path} alt="" style={{width:'200px', 
+                        border:'1px solid yellow', borderRadius: '10px'}}/>
                 </div>
                 <h3>{data.title}</h3>
                 <p>{data.release_date}</p>
@@ -17,3 +29,5 @@ export const Movies = () => {
     </div>
   );
 };
+
+export default Movies
